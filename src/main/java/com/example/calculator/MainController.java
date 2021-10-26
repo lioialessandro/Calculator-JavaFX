1ef16d19844f13b96b1eeef0e9071760fe4d3113
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2021-10-26 12:50:31 +0200

    Keyboard support

--- src/main/java/com/example/calculator/MainController.java
+++ src/main/java/com/example/calculator/MainController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 
 public class MainController {
     String number1 = "", number2 = "", operation = "";
@@ -15,24 +16,49 @@ public class MainController {
     protected void onNumberButtonClick(ActionEvent event) {
         Node node = (Node) event.getSource();
         String userData = (String) node.getUserData();
-        if (operation.isEmpty()) number1 += userData;
-        else number2 += userData;
-        operationLabel.setText("%s %s %s".formatted(number1, operation, number2));
+        handleDigitPress(userData);
     }
 
     @FXML
     protected void onOperationButtonClick(ActionEvent event) {
-        if (number1.isEmpty()) {
-            operationLabel.setText("Invalid syntax (the first number is empty)");
-        } else {
-            Node node = (Node) event.getSource();
-            operation = (String) node.getUserData();
-            operationLabel.setText("%s %s %s".formatted(number1, operation, number2));
-        }
+        Node node = (Node) event.getSource();
+        operation = (String) node.getUserData();
+        handleOperationPress(operation);
     }
 
     @FXML
-    public void onEqualButtonClick() {
+    protected void onEqualButtonClick() {
+        calculate();
+    }
+
+    @FXML
+    protected void onDeleteButtonClick() {
+        handleDeletePress();
+    }
+
+    @FXML
+    protected void onKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case DIGIT0, DIGIT1, DIGIT2, DIGIT3, DIGIT4, DIGIT5, DIGIT6, DIGIT7, DIGIT8, DIGIT9, NUMPAD0, NUMPAD1, NUMPAD2, NUMPAD3, NUMPAD4, NUMPAD5, NUMPAD6, NUMPAD7, NUMPAD8, NUMPAD9 -> handleDigitPress(keyEvent.getText());
+            case ADD, SUBTRACT, SLASH, MULTIPLY -> handleOperationPress(keyEvent.getText());
+            case EQUALS, ENTER -> calculate();
+            case DELETE, BACK_SPACE -> handleDeletePress();
+        }
+    }
+
+    private void handleDigitPress(String digit) {
+        if (operation.isEmpty()) number1 += digit;
+        else number2 += digit;
+        operationLabel.setText("%s %s %s".formatted(number1, operation, number2));
+    }
+
+    private void handleOperationPress(String _operation) {
+        operation = _operation;
+        if (number1.isEmpty()) operationLabel.setText("Invalid syntax (the first number is empty)");
+        else operationLabel.setText("%s %s %s".formatted(number1, operation, number2));
+    }
+
+    private void calculate() {
         if (number1.isEmpty() || number2.isEmpty() || operation.isEmpty()) {
             operationLabel.setText("Invalid syntax");
         }
@@ -53,8 +79,7 @@ public class MainController {
         operation = "";
     }
 
-    @FXML
-    public void onDeleteButtonClick() {
+    private void handleDeletePress() {
         if (operation.isEmpty()) {
             if (number1.length() > 1)
                 number1 = number1.substring(0, number1.length() - 1);
